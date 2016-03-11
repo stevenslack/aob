0dde52784f3693e49dcf8d10f42178b31d76469a
Steven Slack <steven@s2webpress.com>
2016-03-11 11:34:49 -0500

    added aob fonts

--- page.php
+++ page.php
@@ -7,17 +7,14 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'templates/content', 'page' ); ?>
+			<?php get_template_part( 'templates/content', 'page' ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+		<?php endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
